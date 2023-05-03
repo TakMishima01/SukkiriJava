3cf8273ef796c001a3f66a2537182bb8d1f45f2f
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-05-03 11:02:19 +0900

    "[TIL]継承について"

--- src/スッキリJava/Hero.java
+++ src/スッキリJava/Hero.java
@@ -2,43 +2,42 @@ package スッキリJava;
 
 public class Hero {
 //	属性の定義
-	String name;
+	String name = "ミナト";
 	int hp;
 	Sword sword;
 //	コンストラクタ定義
-	public Hero(String name) {
+	public Hero() {
 		this.hp = 100;
-		this.name = name;
 	}
 //	コンストラクタのオーバーロード
-	public Hero() {
-		this( "ダミー");
-	}
+//	public Hero() {
+//		this( "ダミー");
+//	}
 //	操作の定義
-	public void attack() {
-		System.out.println(this.name + "は"+ this.sword.name +"で攻撃した！");
-		System.out.println("敵に５ポイントのダメージをあたえた！");
+	public void attack(Matango m1) {
+		System.out.println(this.name + "の攻撃！");
+		m1.hp -= 5;
+		System.out.println("お化けキノコ" + m1.suffix + "５ポイントのダメージをあたえた！");
 	}
-	public void sleep() {
-		this.hp = 100;
-		System.out.println(this.name + "は、眠って回復した！");
-	}
-	public void sit(int sec) {
-		this.hp += sec;
-		System.out.println(this.name + "は、" + sec + "秒座った！");
-		System.out.println("HPが" + sec + "ポイント回復した");
-	}
-	public void slip() {
-		this.hp -= 5;
-		System.out.println(this.name + "は、転んだ！");
-		System.out.println("5のダメージ！");
-	}
+	
 	public void run() {
 		System.out.println(this.name + "は、逃げ出した！");
-		System.out.println("GAMEOVER");
-		System.out.println("最終HPは" + this.hp + "でした");
 	}
-	
-	
+//	public void sleep() {
+//		this.hp = 100;
+//		System.out.println(this.name + "は、眠って回復した！");
+//	}
+//	public void sit(int sec) {
+//		this.hp += sec;
+//		System.out.println(this.name + "は、" + sec + "秒座った！");
+//		System.out.println("HPが" + sec + "ポイント回復した");
+//	}
+//	public void slip() {
+//		this.hp -= 5;
+//		System.out.println(this.name + "は、転んだ！");
+//		System.out.println("5のダメージ！");
+//	}
+//	
+//	
 
 }
--- src/スッキリJava/Main.java
+++ src/スッキリJava/Main.java
@@ -3,6 +3,10 @@ package スッキリJava;
 public class Main {
 
 	public static void main(String[] args) {
+//		スーパーヒーロー生成
+		SuperHero sh = new SuperHero();
+		sh.run();
+		
 //		剣の生成
 		Sword s = new Sword();
 //		フィールドに初期値をセット
@@ -10,7 +14,8 @@ public class Main {
 		s.damage = 10;
 		
 //		勇者1の生成
-		Hero h1 = new Hero("ミナト");
+		Hero h1 = new Hero();
+		h1.run();
 //		フィールドに初期値をセット
 		h1.sword = s;
 		System.out.println("現在の武器は" + h1.sword.name);
@@ -27,18 +32,22 @@ public class Main {
 		w.name = "スガワラ";
 		w.hp = 50;
 //		メソッドの呼び出し
+		System.out.println(h1.hp);
 		w.heal(h1);
 		w.heal(h2);
 		w.heal(h2);
+		System.out.println(h1.hp);
 		
 //		お化けキノコAの生成
-		Matango m1 = new Matango();
+		Matango m1 = new Matango('A');
 		m1.hp = 50;
-		m1.suffix = 'A';
+
 //		お化けキノコBの生成
-		Matango m2 = new Matango();
-		m2.hp = 48;
-		m2.suffix = 'B';
+		PoisonMatango pm1 = new PoisonMatango('A');
+		pm1.hp = 48;
+		System.out.println(h1.hp);
+		pm1.attack(h1);
+		System.out.println(h1.hp);
 //		勇者のメソッドを呼び出してゆく
 //		h.sit(5);
 //		h.slip();
@@ -46,10 +55,10 @@ public class Main {
 //		h.run();
 		
 //		冒険のはじまり
-		h1.slip();
-		m1.run();
-		m2.run();
-		h2.attack();
+//		h1.slip();
+//		m1.run();
+//		m2.run();
+//		h2.attack();
 	}
 
 }
